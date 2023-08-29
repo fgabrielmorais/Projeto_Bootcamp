a85161ceb055154008cbe7a6663880e16f847458
fgabrielmorais <gabrielgamer.aleatoriow@gmail.com>
2023-08-28 22:42:51 -0300

    página de login finaliazda

---
+++ app.js
@@ -0,0 +1,50 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+
+
+
+//elevação do placeholder
+inputs.forEach((inp) => {
+    inp.addEventListener("focus", () => {
+        inp.classList.add("active");
+    });
+    inp.addEventListener("blur", () => {
+        if(inp.value != "") return;
+        inp.classList.remove("active");
+
+    });
+});
+
+
+//mudança de classe
+toggle_btn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        main.classList.toggle("sign-up-mode");
+
+    });
+});
+
+
+
+//spans do carousel e  mudança de imagem
+function moveSlider(){
+    let index = this.dataset.value;
+    let currentImage = document.querySelector(`.img-${index}`);
+
+    images.forEach((img) => img.classList.remove("show"));
+    currentImage.classList.add("show");
+
+    const textSlider = document.querySelector(".text-group");
+    textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+    bullets.forEach((bull) => bull.classList.remove("active"));
+    this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+    bullet.addEventListener("click", moveSlider);
+});
